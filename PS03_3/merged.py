a771d8c1880b89b364b743497c19c4fde149be0e
Ford-cliff <202480086@psu.palawan.edu.ph>
2025-01-29 23:02:18 +0800

    Delete PS03_3/merged.py

--- PS03_3/merged.py
+++
@@ -1,3 +0,0 @@
-with open("file1.txt", "r") as file1, open("file2.txt", "r") as file2, open("merged.txt", "w") as merged:
-    merged.write(file1.read() + "\n" + file2.read())
-d() + "\n" + file2.read())
