97794475dc86dca483c23350fc02c90610c259d3
Ford-cliff <202480086@psu.palawan.edu.ph>
2025-01-29 23:01:56 +0800

    Delete PS03_3/find_and_replace.py

--- PS03_3/find_and_replace.py
+++
@@ -1,8 +0,0 @@
-old_word, new_word = "old", "new"
-with open("text.txt", "r") as file:
-    content = file.read()
-content = content.replace(old_word, new_word)
-with open("text.txt", "w") as file:
-    file.write(content)
-") as file:
-    file.write(content)
