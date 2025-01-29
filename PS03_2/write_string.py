56ef3a0666805b4c4ec81be20660dd6ff551dd7d
Ford-cliff <202480086@psu.palawan.edu.ph>
2025-01-29 22:41:59 +0800

    Delete PS03_2/write_string.py

--- PS03_2/write_string.py
+++
@@ -1,6 +0,0 @@
-lines = ["First line", "Second line", "Third line"]
-with open("strings.txt", "w") as file:
-    for line in lines:
-        file.write(line + "\n")
-ine in lines:
-        file.write(line + "\n")
